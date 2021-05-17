803d57dec98b3c75bcaf15d9339208159acd3f29
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-05-18 01:37:19 +0700

    Add two hyperparameters

--- train_segmentation.py
+++ train_segmentation.py
@@ -17,8 +17,6 @@ def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epo
                                                                    included_datasets=included_datasets,
                                                                    excluded_datasets=excluded_datasets)
 
-    # TODO: split across different classes (do that after bugfix with the W&B logging)
-    # TODO: we may use a for loop like -> for class_name in class_names: subsets = split_data(...., class_name ,....)
     subsets = split_data(img_paths=img_paths,
                          ann_paths=ann_paths,
                          dataset_names=dataset_names,
@@ -45,8 +43,6 @@ def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epo
     val_loader = DataLoader(datasets['val'], batch_size=batch_size, num_workers=num_workers)
     test_loader = DataLoader(datasets['test'], batch_size=batch_size, num_workers=num_workers)
 
-    # TODO: download covid_segmentation_logging
-    # TODO: aws s3 cp --recursive s3://qtr-ml-projects/prj-covid-scoring/dataset/covid_segmentation_logging/ ./dataset/covid_segmentation_logging/
     logging_dir = dataset_dir + '_logging'
     img_paths_logging, ann_paths_logging, dataset_names_logging = read_supervisely_project(sly_project_dir=logging_dir,
                                                                                            included_datasets=included_datasets,
@@ -73,26 +69,25 @@ def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epo
 
 
 if __name__ == '__main__':
-    # TODO: add other key options if needed
     parser = argparse.ArgumentParser(description='Segmentation pipeline')
-    parser.add_argument('--dataset_dir', default='dataset/covid_segmentation', type=str,
-                        help='dataset/covid_segmentation or dataset/lungs_segmentation')
+    parser.add_argument('--dataset_dir', default='dataset/covid_segmentation', type=str, help='dataset/covid_segmentation or dataset/lungs_segmentation')
     parser.add_argument('--class_name', default='COVID-19', type=str, help='COVID-19 or Lungs')
     parser.add_argument('--input_size', nargs='+', default=(512, 512), type=int)
     parser.add_argument('--device', default='cpu', type=str, help='cuda or cpu')
-    parser.add_argument('--model_name', default='Unet', type=str,
-                        help='Unet, Unet++, DeepLabV3, DeepLabV3+, FPN, Linknet, or PSPNet')
+    parser.add_argument('--model_name', default='Unet', type=str, help='Unet, Unet++, DeepLabV3, DeepLabV3+, FPN, Linknet, or PSPNet')
     parser.add_argument('--encoder_name', default='resnet18', type=str)
     parser.add_argument('--encoder_weights', default='imagenet', type=str, help='imagenet, ssl or swsl')
-    parser.add_argument('--batch_size', default=4, type=int)
-    parser.add_argument('--epochs', default=2, type=int)
+    parser.add_argument('--batch_size', default=8, type=int)
+    parser.add_argument('--lr', default=0.0001, type=float)                 # TODO: add usage of LR in the code
+    parser.add_argument('--epochs', default=5, type=int)
+    parser.add_argument('--monitor_metric', default='iou_score', type=str)  # TODO: add usage of monitor_metric in the code
     parser.add_argument('--save_dir', default='models', type=str)
     parser.add_argument('--included_datasets', default=None, type=str)
     parser.add_argument('--excluded_datasets', default=None, type=str)
-    parser.add_argument('--wandb_project_name', default='my_test_project', type=str)
+    parser.add_argument('--wandb_project_name', default='test_project', type=str)
     parser.add_argument('--wandb_api_key', default='b45cbe889f5dc79d1e9a0c54013e6ab8e8afb871', type=str)
     args = parser.parse_args()
-    args.excluded_datasets = ['covid-chestxray-dataset', 'COVID-19-Radiography-Database']
+    # args.excluded_datasets = ['covid-chestxray-dataset', 'COVID-19-Radiography-Database']     # Used only for debugging
     main(args.dataset_dir, args.model_name, args.encoder_name, args.encoder_weights, args.batch_size,
          args.epochs, args.class_name, args.input_size, args.excluded_datasets, args.included_datasets,
          args.wandb_project_name, args.wandb_api_key)
