31f43fd161c76053a8ea669804c8223a17295efb
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-05-24 01:20:44 +0700

    Add loss and optimizer to the training pipeline

--- train_segmentation.py
+++ train_segmentation.py
@@ -10,8 +10,8 @@ from tools.supervisely_tools import read_supervisely_project
 from tools.data_processing_tools import split_data, covid_segmentation_labels
 
 
-def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epochs, class_name,
-         input_size, save_dir, excluded_datasets, included_datasets, lr, monitor_metric, wandb_project_name, wandb_api_key):
+def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epochs, class_name, input_size, save_dir,
+         excluded_datasets, included_datasets, loss, optimizer, lr, monitor_metric, wandb_project_name, wandb_api_key):
 
     img_paths, ann_paths, dataset_names = read_supervisely_project(sly_project_dir=dataset_dir,
                                                                    included_datasets=included_datasets,
@@ -37,8 +37,7 @@ def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epo
         datasets[subset_name] = dataset
 
     # If debug is frozen, use num_workers = 0
-    # num_workers = 4 * device_count()
-    num_workers = 0
+    num_workers = 8 * device_count()
     train_loader = DataLoader(datasets['train'], batch_size=batch_size, num_workers=num_workers)
     val_loader = DataLoader(datasets['val'], batch_size=batch_size, num_workers=num_workers)
     test_loader = DataLoader(datasets['test'], batch_size=batch_size, num_workers=num_workers)
@@ -61,6 +60,8 @@ def main(dataset_dir, model_name, encoder_name, encoder_weights, batch_size, epo
                               batch_size=batch_size,
                               epochs=epochs,
                               class_name=class_name,
+                              loss=loss,
+                              optimizer=optimizer,
                               lr=lr,
                               input_size=input_size,
                               monitor_metric=monitor_metric,
@@ -76,13 +77,14 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_dir', default='dataset/covid_segmentation', type=str, help='dataset/covid_segmentation or dataset/lungs_segmentation')
     parser.add_argument('--class_name', default='COVID-19', type=str, help='COVID-19 or Lungs')
     parser.add_argument('--input_size', nargs='+', default=(512, 512), type=int)
-    parser.add_argument('--device', default='cpu', type=str, help='cuda or cpu')
     parser.add_argument('--model_name', default='Unet', type=str, help='Unet, Unet++, DeepLabV3, DeepLabV3+, FPN, Linknet, or PSPNet')
     parser.add_argument('--encoder_name', default='resnet18', type=str)
     parser.add_argument('--encoder_weights', default='imagenet', type=str, help='imagenet, ssl or swsl')
-    parser.add_argument('--batch_size', default=8, type=int)
-    parser.add_argument('--lr', default=0.0001, type=float)
-    parser.add_argument('--epochs', default=5, type=int)
+    parser.add_argument('--batch_size', default=4, type=int)
+    parser.add_argument('--loss', default='Dice', type=str, help='Dice, Jaccard, BCE or BCE_with_logits')
+    parser.add_argument('--optimizer', default='Adam', type=str, help='SGD, Adam, AdamW, RMSprop, Adam_amsgrad or AdamW_amsgrad')
+    parser.add_argument('--lr', default=0.001, type=float)
+    parser.add_argument('--epochs', default=2, type=int)
     parser.add_argument('--monitor_metric', default='fscore', type=str)
     parser.add_argument('--save_dir', default='models', type=str)
     parser.add_argument('--included_datasets', default=None, type=str)
@@ -90,7 +92,9 @@ if __name__ == '__main__':
     parser.add_argument('--wandb_project_name', default='covid_segmentation', type=str)
     parser.add_argument('--wandb_api_key', default='b45cbe889f5dc79d1e9a0c54013e6ab8e8afb871', type=str)
     args = parser.parse_args()
-    args.excluded_datasets = ['covid-chestxray-dataset', 'COVID-19-Radiography-Database', 'Figure1-COVID-chestxray-dataset']     # Used only for debugging
+    # args.excluded_datasets = ['covid-chestxray-dataset', 'COVID-19-Radiography-Database', 'Figure1-COVID-chestxray-dataset']     # Used only for debugging
+    args.excluded_datasets = ['covid-chestxray-dataset', 'COVID-19-Radiography-Database', 'Figure1-COVID-chestxray-dataset', 'rsna_normal', 'chest_xray_normal']     # Used only for debugging
+
     main(dataset_dir=args.dataset_dir,
          model_name=args.model_name,
          encoder_name=args.encoder_name,
@@ -102,6 +106,8 @@ if __name__ == '__main__':
          save_dir=args.save_dir,
          included_datasets=args.included_datasets,
          excluded_datasets=args.excluded_datasets,
+         loss=args.loss,
+         optimizer=args.optimizer,
          lr=args.lr,
          monitor_metric=args.monitor_metric,
          wandb_project_name=args.wandb_project_name,
