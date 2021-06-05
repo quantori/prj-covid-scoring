04b28f785633fe6a3465c6d093f7ae47a23f4bc9
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-06-06 02:18:05 +0700

    Fix EarlyStopping class

--- tools/utils.py
+++ tools/utils.py
@@ -1,37 +1,30 @@
-import copy
 import numpy as np
+from typing import Dict
 
 
 class EarlyStopping:
     def __init__(self,
-                 model,
                  monitor_metric: str = None,
-                 mode: str = None,
                  patience: int = None,
                  min_delta: float = 0):
-        assert mode in ['min', 'max'], 'incorrect mode {}'.format(mode)
-        assert min_delta >= 0, 'delta must be nonnegative'
-        assert patience >= 0, 'patience should be nonnegative'
+        assert min_delta >= 0, 'min_delta must be non-negative'
+        assert patience >= 0, 'patience must be non-negative'
         assert monitor_metric is not None, 'monitor metric should have some value'
 
-        self.best_model = copy.deepcopy(model)
         self.monitor_metric = monitor_metric
-        self.mode = mode
         self.patience = patience
         self.min_delta = min_delta
-        self.optimal_value = -np.inf if mode == 'max' else np.inf
-
+        self.optimal_value, self.mode = (np.inf, 'min') if 'loss' in monitor_metric else (-np.inf, 'max')
         self.counter = 0
         self.early_stop = False
 
-    def __call__(self, metrics, model):
+    def __call__(self, metrics: Dict[str, float]):
         score = metrics.get(self.monitor_metric)
         assert score is not None, '{} doesn\'t exist in metrics'.format(self.monitor_metric)
 
         if self.is_better_optimum(score):
             self.counter = 0
             self.optimal_value = score
-            self.best_model = copy.deepcopy(model)
         else:
             self.counter += 1
 
@@ -40,12 +33,12 @@ class EarlyStopping:
 
     def is_better_optimum(self, score):
         if self.mode == 'max':
-            if abs(score - self.optimal_value) > self.min_delta:
+            if score > self.optimal_value and (abs(score - self.optimal_value) > self.min_delta):
                 return True
             else:
                 return False
         if self.mode == 'min':
-            if abs(score - self.optimal_value) > self.min_delta:
+            if score < self.optimal_value and (abs(score - self.optimal_value) > self.min_delta):
                 return True
             else:
                 return False
