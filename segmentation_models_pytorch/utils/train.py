19d34a57fb372d4f0ee4cf137771e0bab7ec53a2
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-06-27 19:27:35 +0700

    Fix naming and calculate metrics (fixes are still needed)

--- segmentation_models_pytorch/utils/train.py
+++ segmentation_models_pytorch/utils/train.py
@@ -6,12 +6,12 @@ from .meter import AverageValueMeter
 
 class Epoch:
 
-    def __init__(self, model, loss1, loss2, w1, w2, metrics, stage_name, device='cpu', verbose=True):
+    def __init__(self, model, loss_seg, loss_cls, weight_seg, weight_cls, metrics, stage_name, device='cpu', verbose=True):
         self.model = model
-        self.loss1 = loss1
-        self.loss2 = loss2
-        self.w1 = w1
-        self.w2 = w2
+        self.loss_seg = loss_seg
+        self.loss_cls = loss_cls
+        self.weight_seg = weight_seg
+        self.weight_cls = weight_cls
 
         self.metrics = metrics
         self.stage_name = stage_name
@@ -22,9 +22,9 @@ class Epoch:
 
     def _to_device(self):
         self.model.to(self.device)
-        self.loss1.to(self.device)
-        if not (self.loss2 is None):
-            self.loss2.to(self.device)
+        self.loss_seg.to(self.device)
+        if not (self.loss_cls is None):
+            self.loss_cls.to(self.device)
 
         for metric in self.metrics:
             metric.to(self.device)
@@ -45,35 +45,47 @@ class Epoch:
         self.on_epoch_start()
 
         logs = {}
-        loss_meter1 = AverageValueMeter()
-        loss_meter2 = AverageValueMeter()
+        loss_meter_seg = AverageValueMeter()
+        loss_meter_cls = AverageValueMeter()
         metrics_meters = {metric.__name__: AverageValueMeter() for metric in self.metrics}
 
         with tqdm(dataloader, desc=self.stage_name, file=sys.stdout, disable=not (self.verbose)) as iterator:
             for x, y, z in iterator:
                 x, y, z = x.to(self.device), y.to(self.device), z.to(self.device)
-                loss, y_pred = self.batch_update(x, y, z)
+                loss, pred = self.batch_update(x, y, z)
 
                 if isinstance(loss, tuple):
-                    # update loss logs
-                    loss1, loss2 = loss
-                    loss_meter1.add(loss1.cpu().detach().numpy())
-                    loss_meter2.add(loss2.cpu().detach().numpy())
-
-                    loss_logs = {self.loss1.__name__: loss_meter1.mean, self.loss2.__name__: loss_meter2.mean}
+                    # Update loss logs
+                    loss_seg, loss_cls = loss
+                    loss_meter_seg.add(loss_seg.cpu().detach().numpy())
+                    loss_meter_cls.add(loss_cls.cpu().detach().numpy())
+                    loss_logs = {self.loss_seg.__name__: loss_meter_seg.mean, self.loss_cls.__name__: loss_meter_cls.mean}
                     logs.update(loss_logs)
+
+                    # FIXME (David): update *_seg and *_cls metrics, debug and then test this section
+                    # FIXME (David): seg metrics are only updated now
+                    # Update metric logs
+                    for metric_fn in self.metrics:
+                        metric_value_seg = metric_fn(pred[0], y).cpu().detach().numpy()
+                        metrics_meters[metric_fn.__name__].add(metric_value_seg)
+                        metric_value_cls = metric_fn(pred[1], z).cpu().detach().numpy()
+                        metrics_meters[metric_fn.__name__].add(metric_value_cls)
+                    metrics_logs = {k: v.mean for k, v in metrics_meters.items()}
+                    logs.update(metrics_logs)
+
                 else:
+                    # Update loss logs
                     loss_value = loss.cpu().detach().numpy()
-                    loss_meter1.add(loss_value)
-                    loss_logs = {self.loss1.__name__: loss_meter1.mean}
+                    loss_meter_seg.add(loss_value)
+                    loss_logs = {self.loss_seg.__name__: loss_meter_seg.mean}
                     logs.update(loss_logs)
 
-                # update metrics logs
-                for metric_fn in self.metrics:
-                    metric_value = metric_fn(y_pred, y).cpu().detach().numpy()
-                    metrics_meters[metric_fn.__name__].add(metric_value)
-                metrics_logs = {k: v.mean for k, v in metrics_meters.items()}
-                logs.update(metrics_logs)
+                    # Update metric logs
+                    for metric_fn in self.metrics:
+                        metric_value = metric_fn(pred, y).cpu().detach().numpy()
+                        metrics_meters[metric_fn.__name__].add(metric_value)
+                    metrics_logs = {k: v.mean for k, v in metrics_meters.items()}
+                    logs.update(metrics_logs)
 
                 if self.verbose:
                     s = self._format_logs(logs)
@@ -83,13 +95,13 @@ class Epoch:
 
 
 class TrainEpoch(Epoch):
-    def __init__(self, model, loss1, loss2, w1, w2, metrics, optimizer, device='cpu', verbose=True):
+    def __init__(self, model, loss_seg, loss_cls, weight_seg, weight_cls, metrics, optimizer, device='cpu', verbose=True):
         super().__init__(
             model=model,
-            loss1=loss1,
-            loss2=loss2,
-            w1=w1,
-            w2=w2,
+            loss_seg=loss_seg,
+            loss_cls=loss_cls,
+            weight_seg=weight_seg,
+            weight_cls=weight_cls,
             metrics=metrics,
             stage_name='train',
             device=device,
@@ -105,13 +117,18 @@ class TrainEpoch(Epoch):
         prediction = self.model.forward(x)
 
         if isinstance(prediction, tuple):
-            prediction, aux_pred = prediction
-            loss = self.loss1(prediction, y) * self.w1 + self.loss2(aux_pred, z) * self.w2
+            pred_seg, pred_cls = prediction
+            # TODO (David): Think of rebalancing weights when one of the losses is getting very small
+            # TODO (David): Dynamic weights might be used here (https://arxiv.org/abs/2009.01717)
+            # TODO (David): Log to W&B loss, loss_seg, and loss_cls
+            # TODO (David): Log to W&B weight_seg and weight_cls, if they are dynamic (not constant)
+            #
+            loss = self.loss_seg(pred_seg, y) * self.weight_seg + self.loss_cls(pred_cls, z) * self.weight_cls
             loss.backward()
             self.optimizer.step()
-            return (self.loss1(prediction, y), self.loss2(aux_pred, z)), prediction
+            return (self.loss_seg(pred_seg, y), self.loss_cls(pred_cls, z)), prediction
         else:
-            loss = self.loss1(prediction, y)
+            loss = self.loss_seg(prediction, y)
             loss.backward()
             self.optimizer.step()
             return loss, prediction
@@ -119,13 +136,13 @@ class TrainEpoch(Epoch):
 
 class ValidEpoch(Epoch):
 
-    def __init__(self, model, loss1, loss2, w1, w2, metrics, stage_name, device='cpu', verbose=True):
+    def __init__(self, model, loss_seg, loss_cls, weight_seg, weight_cls, metrics, stage_name, device='cpu', verbose=True):
         super().__init__(
             model=model,
-            loss1=loss1,
-            loss2=loss2,
-            w1=w1,
-            w2=w2,
+            loss_seg=loss_seg,
+            loss_cls=loss_cls,
+            weight_seg=weight_seg,
+            weight_cls=weight_cls,
             metrics=metrics,
             stage_name=stage_name,
             device=device,
@@ -140,8 +157,8 @@ class ValidEpoch(Epoch):
             prediction = self.model.forward(x)
 
             if isinstance(prediction, tuple):
-                prediction, aux_pred = prediction
-                return (self.loss1(prediction, y), self.loss2(aux_pred, z)), prediction
+                pred_seg, pred_cls = prediction
+                return (self.loss_seg(pred_seg, y), self.loss_cls(pred_cls, z)), prediction
             else:
-                loss = self.loss1(prediction, y)
+                loss = self.loss_seg(prediction, y)
                 return loss, prediction
