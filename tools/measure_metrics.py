8093f2b3457f5a38addd1d5bb263ec8fdbddfb03
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-08-29 20:23:51 +0700

    Update metric computation

--- tools/measure_metrics.py
+++ tools/measure_metrics.py
@@ -1,11 +1,12 @@
 import os
 import argparse
 from typing import List
+from pathlib import Path
 
 import pandas as pd
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 
-from tools.utils import rmse_parameters, measure_metrics, compute_df_metrics
+from tools.utils import rmse_parameters, compute_metrics
 
 
 def main(model_outputs: pd.DataFrame,
@@ -13,11 +14,11 @@ def main(model_outputs: pd.DataFrame,
          model_columns: List,
          save_dir: str,
          save_name: str) -> None:
-    metrics = {'mae': mean_absolute_error,
-               'mse': mean_squared_error,
-               'rmse': rmse_parameters(squared=False),
-               'r2': r2_score}
-    df_metrics = compute_df_metrics(model_outputs, gt_column, model_columns, metrics)
+    metrics = {'MAE': mean_absolute_error,
+               'MSE': mean_squared_error,
+               'RMSE': rmse_parameters(squared=False),
+               'R2': r2_score}
+    df_metrics = compute_metrics(model_outputs, gt_column, model_columns, metrics)
     os.makedirs(save_dir) if not os.path.exists(save_dir) else False
     save_path = os.path.join(save_dir, save_name)
     df_metrics.to_csv(save_path)
@@ -25,13 +26,34 @@ def main(model_outputs: pd.DataFrame,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--net_comparison_csv', type=str)
-    parser.add_argument('--save_dir', default='dataset/inference_outputs', type=str)
-    parser.add_argument('--save_name', default='model_metrics.csv', type=str)
+    parser.add_argument('--net_comparison_csv', required=True, type=str)
+    parser.add_argument('--dataset', default='all', type=str, help='all or a specific dataset name')
+    parser.add_argument('--label', default='COVID-19', type=str, help='all, Normal, or COVID-19')
+    parser.add_argument('--subset', default='train', type=str, help='all, train, val, or test')
+    parser.add_argument('--gt_column', default='GT', type=str)
+    parser.add_argument('--model_columns', nargs='+', default=['Our', 'BSNet', 'CovidNet'], type=str)
+    parser.add_argument('--save_dir', default='resources', type=str)
+    parser.add_argument('--save_name', default='metrics.csv', type=str)
     args = parser.parse_args()
 
-    model_outputs = pd.read_csv(args.net_comparison_csv)
-    gt_column = 'GT'
-    model_columns = ['Our', 'BSNet', 'CovidNet']
+    df_src = pd.read_csv(args.net_comparison_csv)
+    df_agg = df_src.copy(deep=True)
 
-    main(model_outputs, gt_column, model_columns, args.save_dir, args.save_name)
+    if args.dataset != 'all':
+        dataset_names = list(df_src['Dataset'].unique())
+        assert args.dataset in dataset_names, 'There is no dataset {:s}'.format(args.dataset)
+        df_agg = df_agg[df_agg['Dataset'] == args.dataset]
+
+    if args.label != 'all':
+        df_agg = df_agg[df_agg['Label'] == args.label]
+
+    if args.subset != 'all':
+        df_agg = df_agg[df_agg['Subset'] == args.subset]
+
+    df_agg.reset_index(drop=True, inplace=True)
+
+    base_name = str(Path(args.save_name).stem)
+    base_ext = str(Path(args.save_name).suffix)
+    args.save_name = '_'.join([base_name, args.label.lower(), args.subset.lower()]) + base_ext
+
+    main(df_src, args.gt_column, args.model_columns, args.save_dir, args.save_name)
