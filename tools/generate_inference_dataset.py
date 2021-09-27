c8b3a9b868b74585298cd939e5c2eeb62e6c9b97
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-09-27 23:46:12 +0700

    Revise the generation of the inference dataset

--- tools/generate_inference_dataset.py
+++ tools/generate_inference_dataset.py
@@ -12,16 +12,20 @@ from tools.supervisely_tools import read_supervisely_project, convert_ann_to_mas
 
 
 def main(args):
-    img_paths, ann_paths, dataset_names = read_supervisely_project(sly_project_dir=args.dataset_dir,
-                                                                   included_datasets=args.included_datasets,
-                                                                   excluded_datasets=args.excluded_datasets)
-    subsets = split_data(img_paths=img_paths,
-                         ann_paths=ann_paths,
-                         dataset_names=dataset_names,
-                         class_name=args.class_name,
-                         seed=11,
-                         ratio=args.ratio,
-                         normal_datasets=args.normal_datasets)
+    img_paths, ann_paths, dataset_names = read_supervisely_project(
+        sly_project_dir=args.dataset_dir,
+        included_datasets=args.included_datasets,
+        excluded_datasets=args.excluded_datasets,
+    )
+    subsets = split_data(
+        img_paths=img_paths,
+        ann_paths=ann_paths,
+        dataset_names=dataset_names,
+        class_name=args.class_name,
+        seed=11,
+        ratio=args.ratio,
+        normal_datasets=args.normal_datasets,
+    )
 
     for subset in subsets:
         for dataset in dataset_names:
@@ -32,17 +36,34 @@ def main(args):
 
     if set(args.covid_datasets) & set(args.normal_datasets):
         elements = set(args.covid_datasets) & set(args.normal_datasets)
-        print('\033[91m' + 'There are common elements in both dataset lists '
-                           'that may cause an incorrect estimation of accuracy: {}\n'.format(elements) + '\033[0m')
+        print(
+            '\033[91m' + 'There are common elements in both dataset lists '
+            'that may cause an incorrect estimation of accuracy: {}\n'.format(elements)
+            + '\033[0m'
+        )
 
     metadata_df = pd.DataFrame()
     for subset in subsets:
         img_paths, ann_paths = subsets[subset]
-        metadata = {'dataset': [], 'filename': [], 'Inaccurate labelling': [], 'Score R': [], 'Score D': [],
-                    'Poor quality D': [], 'Poor quality R': [], 'ann_found': [], 'subset': [], 'label': [],
-                    'Normal': []}
+        metadata = {
+            'dataset': [],
+            'filename': [],
+            'Inaccurate labelling': [],
+            'Score R': [],
+            'Score D': [],
+            'Poor quality D': [],
+            'Poor quality R': [],
+            'ann_found': [],
+            'subset': [],
+            'label': [],
+            'Normal': [],
+        }
 
-        for idx in tqdm(range(len(img_paths)), desc='Processing of {:s} dataset'.format(subset), unit=' images'):
+        for idx in tqdm(
+            range(len(img_paths)),
+            desc='Processing of {:s} dataset'.format(subset),
+            unit=' images',
+        ):
             image_path = img_paths[idx]
             ann_path = ann_paths[idx]
             image_path = os.path.normpath(image_path)
@@ -61,17 +82,21 @@ def main(args):
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             mask = convert_ann_to_mask(ann_path=ann_path, class_name=args.class_name)
 
-            img_output = os.path.join(args.output_dir, subset, dataset_name, 'img', filename)
-            mask_output = os.path.join(args.output_dir, subset, dataset_name, 'mask', filename)
+            img_output = os.path.join(
+                args.output_dir, subset, dataset_name, 'img', filename
+            )
+            mask_output = os.path.join(
+                args.output_dir, subset, dataset_name, 'mask', filename
+            )
 
             metadata['label'].append(label)
             metadata['subset'].append(subset)
             metadata['dataset'].append(dataset_name)
             metadata['filename'].append(filename)
 
-            extracted_values = extract_ann_score(dataset_name,
-                                                 args.normal_datasets,
-                                                 ann_path)
+            extracted_values = extract_ann_score(
+                dataset_name, args.normal_datasets, ann_path
+            )
 
             for key, value in extracted_values.items():
                 metadata[key].append(value)
@@ -97,14 +122,14 @@ if __name__ == '__main__':
     parser.add_argument('--included_datasets', default=None, type=str)
     parser.add_argument('--excluded_datasets', default=None, type=str)
     parser.add_argument('--ratio', nargs='+', default=(0.8, 0.1, 0.1), type=float, help='train, val, and test sizes')
-    parser.add_argument('--output_dir', default='dataset/output/inference', type=str)
+    parser.add_argument('--output_dir', default='dataset/inference', type=str)
 
     args = parser.parse_args()
     if 'covid' in args.dataset_dir:
         args.class_name = 'COVID-19'
-    elif 'lungs' in args.dataset_dir:
-        args.class_name = 'Lungs'
     else:
-        raise ValueError('There is no class name for dataset {:s}'.format(args.dataset_dir))
+        raise ValueError(
+            'There is no class name for dataset {:s}'.format(args.dataset_dir)
+        )
 
     main(args)
