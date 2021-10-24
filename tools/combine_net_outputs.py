4bc09959f07c453a5ab2437356806776ab8349e7
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2021-10-24 18:43:53 +0700

    Update a default save name

--- tools/combine_net_outputs.py
+++ tools/combine_net_outputs.py
@@ -16,43 +16,43 @@ def combine_inferences(
 ) -> None:
     df_ground_truth = pd.read_csv(ground_truth_csv)
     df_ground_truth = process_gt_metadata(df_ground_truth)
-    df_ground_truth = df_ground_truth.rename(columns={"Score C": "GT"})
+    df_ground_truth = df_ground_truth.rename(columns={'Score C': 'GT'})
 
     df_our = pd.read_csv(our_csv)
-    df_our = df_our.rename(columns={"score": "Our"})
+    df_our = df_our.rename(columns={'score': 'Our'})
 
     df_bsnet = pd.read_csv(bsnet_csv)
-    df_bsnet = df_bsnet.rename(columns={"predicted_score": "BSNet"})
+    df_bsnet = df_bsnet.rename(columns={'predicted_score': 'BSNet'})
 
     df_covid_net = pd.read_csv(covid_net_csv)
-    df_covid_net = df_covid_net.rename(columns={"rounded_geo_score": "CovidNet"})
+    df_covid_net = df_covid_net.rename(columns={'rounded_geo_score': 'CovidNet'})
 
     dfs = [df_our, df_bsnet, df_covid_net]
     result = df_ground_truth
     for df in dfs:
-        result = pd.merge(result, df, on=["filename", "dataset"])
+        result = pd.merge(result, df, on=['filename', 'dataset'])
 
     needed_columns = [
-        "dataset",
-        "filename",
-        "subset",
-        "label",
-        "Score R",
-        "Score D",
-        "GT",
-        "BSNet",
-        "CovidNet",
-        "Our",
+        'dataset',
+        'filename',
+        'subset',
+        'label',
+        'Score R',
+        'Score D',
+        'GT',
+        'BSNet',
+        'CovidNet',
+        'Our',
     ]
-    needed_columns += ["lung_segment_{:d}".format(idx + 1) for idx in range(6)]
+    needed_columns += ['lung_segment_{:d}'.format(idx + 1) for idx in range(6)]
     drop_columns = set(result.columns) - set(needed_columns)
     result = result.drop(list(drop_columns), axis=1)
     result.rename(
         columns={
-            "dataset": "Dataset",
-            "filename": "Filename",
-            "subset": "Subset",
-            "label": "Label",
+            'dataset': 'Dataset',
+            'filename': 'Filename',
+            'subset': 'Subset',
+            'label': 'Label',
         },
         inplace=True,
     )
@@ -62,14 +62,14 @@ def combine_inferences(
     result.to_csv(save_path, index=False)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ground_truth_csv", type=str)
-    parser.add_argument("--our_csv", type=str)
-    parser.add_argument("--bsnet_csv", type=str)
-    parser.add_argument("--covid_net_csv", type=str)
-    parser.add_argument("--save_dir", default="resources", type=str)
-    parser.add_argument("--save_name", default="model_outputs_all.csv", type=str)
+    parser.add_argument('--ground_truth_csv', type=str)
+    parser.add_argument('--our_csv', type=str)
+    parser.add_argument('--bsnet_csv', type=str)
+    parser.add_argument('--covid_net_csv', type=str)
+    parser.add_argument('--save_dir', default='resources', type=str)
+    parser.add_argument('--save_name', default='model_outputs.csv', type=str)
     args = parser.parse_args()
 
     combine_inferences(
